aae2f4c1f3df6eb555e5dd2a89d49d3970625e41
panku43 <152669410+panku43@users.noreply.github.com>
2025-02-06 09:24:31 +0530

    Create git.bash

---
+++ git.bash
@@ -0,0 +1,5 @@
+git init  # Initializes a Git repository
+git add .  # Adds all files to the staging area
+git commit -m "Initial commit"  # Commits the files
+git remote add origin https://github.com/your-username/ssc-exam-assistant.git  # Replace with your repo URL
+git push -u origin master  # Pushes the code to GitHub
